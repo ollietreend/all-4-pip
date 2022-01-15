321460651d938468a230d98580a1cab8bfa5ff16
Ollie Treend <ollie@treend.uk>
2022-01-15 13:39:45 +0000

    Fix a bug which stopped PiP in certain cases
    This commit changes some configruation so that the Chrome extension applies to more pages on the Channel 4 domain. There was a particular scenario where the extension wasn't executing when it should have been.
    
    When navigating from the Channel 4 homepage, to a programme page, and then on to watch an episode, the extension wasn't firing. This was due to the navigation between Programme page => Playback page happening dynamically without a full page reload. This meant the extension didn't execute and re-enable PiP on the `<video>` element on the page.

--- enable-pip.js
+++ enable-pip.js
@@ -1,5 +1,5 @@
 // Wait for the <video> element to be created
-document.arrive("video.vod-player", {onceOnly: true}, function() {
+document.arrive("video.vod-player", {existing: true, fireOnAttributesModification: true}, function() {
   // Re-enable Picture-in-Picture (PiP)
   this.disablePictureInPicture = false;
 });
